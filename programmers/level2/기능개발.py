7e77f1b1aa53d8e4c5e75bf23370f86b380960af
이현수 <cris_lx@naver.com>
2022-10-20 02:48:00 +0900

    index 0 접근 방지, deque 활용

---
+++ programmers/level2/기능개발.py
@@ -0,0 +1,20 @@
+from collections import deque
+
+
+def solution(progresses, speeds):
+    answer = []
+    progresses, speeds = deque(progresses), deque(speeds)
+    while progresses:
+        for i in range(len(progresses)):
+            progresses[i] += speeds[i]
+
+        cnt = 0
+        while progresses and progresses[0] >= 100:
+            cnt += 1
+            progresses.popleft()
+            speeds.popleft()
+
+        if cnt != 0:
+            answer.append(cnt)
+    return answer
+
