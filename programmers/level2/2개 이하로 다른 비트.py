009b4908fbda2782d12d91cf59605f934e1857b7
이현수 <cris_lx@naver.com>
2022-10-28 21:36:08 +0900

    홀수인 경우에 2개의 비트를 바꾸는 아이디어

---
+++ programmers/level2/2개 이하로 다른 비트.py
@@ -0,0 +1,12 @@
+def solution(numbers):
+    answer = []
+    for num in numbers:
+        binary = list('0' + bin(num)[2:])
+        idx = ''.join(binary).rfind('0')
+        binary[idx] = '1'
+
+        if num % 2 == 1:
+            binary[idx + 1] = '0'
+
+        answer.append(int(''.join(binary), 2))
+    return answer
