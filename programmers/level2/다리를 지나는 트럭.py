614e66bc01cfea6e7b00868c3f1c61f43af16446
이현수 <cris_lx@naver.com>
2022-10-31 17:07:20 +0900

    0을 사용해 bridge를 만들어 시간순으로 트럭의 위치 표현

--- programmers/level2/다리를 지나는 트럭.py
+++ programmers/level2/다리를 지나는 트럭.py
@@ -1,13 +1,19 @@
-def solution(bridge_length, weight, truck_weights):
-    answer = 0
-    bridge = [0] * bridge_length
+from collections import deque
 
+
+def solution(bridge_length, weight, truck_weights):
+    truck_weights = deque(truck_weights)
+    answer, total = 0, 0
+    bridge = deque([0] * bridge_length)
     while len(bridge):
         answer += 1
-        bridge.pop(0)
+        total -= bridge.popleft()
+
         if truck_weights:
-            if sum(bridge) + truck_weights[0] <= weight:
-                bridge.append(truck_weights.pop(0))
+            if total + truck_weights[0] <= weight:
+                truck = truck_weights.popleft()
+                bridge.append(truck)
+                total += truck
             else:
                 bridge.append(0)
     return answer
