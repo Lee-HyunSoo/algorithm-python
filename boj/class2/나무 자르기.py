8df31e5f0dcc6715207c31f38987f0c12e3cd5e5
HS <cris_lx@naver.com>
2022-11-24 23:48:10 +0900

    이분 탐색

--- boj/class2/나무 자르기.py
+++ boj/class2/나무 자르기.py
@@ -4,16 +4,17 @@ n, m = map(int, sys.stdin.readline().split())
 tree = list(map(int, sys.stdin.readline().split()))
 start, end = 1, max(tree)
 
+answer = start
 while start <= end:
     mid = (start + end) // 2
-
-    answer = 0
+    tmp = 0
     for t in tree:
         if t >= mid:
-            answer += t - mid
-    if answer < m:
+            tmp += t - mid
+
+    if tmp < m:
         end = mid - 1
     else:
         start = mid + 1
-print(end)
-
+        answer = max(answer, mid)
+print(answer)
