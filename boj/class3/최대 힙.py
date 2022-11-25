90855ab4f6ae5384aaacfff5e52931951183b12d
HS <cris_lx@naver.com>
2022-11-25 23:43:49 +0900

    heap (파이썬은 기본 최소힙)

---
+++ boj/class3/최대 힙.py
@@ -0,0 +1,11 @@
+import sys
+import heapq
+
+n = int(sys.stdin.readline())
+heap = []
+for _ in range(n):
+    x = int(sys.stdin.readline())
+    if x == 0:
+        print(heapq.heappop(heap) * -1 if heap else 0)
+    else:
+        heapq.heappush(heap, -x)
