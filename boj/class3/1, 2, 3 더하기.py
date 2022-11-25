1fef08330ab9d70c7735a4cca53848d5d5b88642
HS <cris_lx@naver.com>
2022-11-25 18:18:34 +0900

    dp

---
+++ boj/class3/1, 2, 3 더하기.py
@@ -0,0 +1,11 @@
+import sys
+
+t = int(sys.stdin.readline())
+
+for _ in range(t):
+    n = int(sys.stdin.readline())
+    dp = [0] * 12
+    dp[1], dp[2], dp[3] = 1, 2, 4
+    for i in range(4, n + 1):
+        dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3]
+    print(dp[n])
