9db94563c7b84e5f396f52eeeb3df6c9bfd056c2
HS <cris_lx@naver.com>
2022-11-28 12:49:40 +0900

    dfs

--- boj/class3/연결 요소의 개수.py
+++ boj/class3/연결 요소의 개수.py
@@ -2,16 +2,14 @@ import sys
 
 
 def dfs(graph, visit, x):
-    visit[x] = True
-    if True not in visit:
-        return
-
     for y in range(len(graph[x])):
         if graph[x][y] == 1 and not visit[y]:
+            visit[y] = True
             dfs(graph, visit, y)
 
 
 n, m = map(int, sys.stdin.readline().split())
+
 graph = [[0] * (n + 1) for _ in range(n + 1)]
 visit = [False] * (n + 1)
 for _ in range(m):
@@ -22,6 +20,7 @@ for _ in range(m):
 answer = 0
 for i in range(1, n + 1):
     if not visit[i]:
+        visit[i] = True
         dfs(graph, visit, i)
         answer += 1
 print(answer)
