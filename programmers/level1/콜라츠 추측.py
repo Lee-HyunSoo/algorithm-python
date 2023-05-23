62db8761dd595d551457df72c85c1512c8736301
HS <cris_lx@naver.com>
2023-05-23 13:01:08 +0900

    if

--- programmers/level1/콜라츠 추측.py
+++ programmers/level1/콜라츠 추측.py
@@ -1,14 +1,13 @@
 def solution(num):
-    answer = 0
-    while True:
+    if num == 1:
+        return 0
+    cnt = 0
+    while cnt < 500:
         if num == 1:
-            return answer
-        if num % 2 == 0:
+            return cnt
+        elif num % 2 == 0:
             num /= 2
-        else:
-            num = (num * 3) + 1
-        answer += 1
-        if answer == 500:
-            return -1
-        if num == 1:
-            return answer
+        elif num % 2 != 0:
+            num = num * 3 + 1
+        cnt += 1
+    return -1
