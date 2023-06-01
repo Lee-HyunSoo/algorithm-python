757b8f8671e8454b5f840e7f750a73ff12b2e99d
HS <cris_lx@naver.com>
2023-06-01 21:43:39 +0900

    prime

--- programmers/level1/소수 찾기.py
+++ programmers/level1/소수 찾기.py
@@ -2,7 +2,7 @@ def solution(n):
     answer = 0
     prime = [False, False] + [True] * (n - 1)
 
-    for i in range(2, n + 1):
+    for i in range(len(prime)):
         if prime[i]:
             answer += 1
             for j in range(i * 2, n + 1, i):
